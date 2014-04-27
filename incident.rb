072e6632d169a12f6747b122c4070c7eac7e4cc4
Andrew Kelly <kellyaj@gmail.com>
2014-04-27 14:39:39 -0500

    changes date key

--- incident.rb
+++ incident.rb
@@ -8,8 +8,10 @@ class Incident
   property :longitude,            String
   property :description,          String
   property :local_identifier,     String
-  property :incident_date,        String
+  property :date,                 String
   property :location_icon,        String
   property :location_description, String
   property :arrest_status,        String
+
+  validates_uniqueness_of :local_identifier
 end
