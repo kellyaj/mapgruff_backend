4ba9d7e10e6ef48f5ec1f26fbb45ca16631ec827
Andrew Kelly <kellyaj@gmail.com>
2014-04-27 14:40:05 -0500

    removes nil check in favor of dm convention

--- lib/incident_creator.rb
+++ lib/incident_creator.rb
@@ -3,10 +3,7 @@ class IncidentCreator
 
   def self.create_chicago_incidents(incident_array)
     incident_array.each do |incident|
-      incident = Incident.first(:local_identifier => incident["local_identifier"])
-      if incident.nil?
         Incident.create(incident)
-      end
     end
   end
 
