8f686b96f203184d0619bef67e2cb65882117df8
Andrew Kelly <kellyaj@gmail.com>
2014-04-27 14:58:49 -0500

    small golf refactor to Monger#standardize

--- lib/incident_monger.rb
+++ lib/incident_monger.rb
@@ -3,16 +3,16 @@ class IncidentMonger
   def self.standardize!(city_config, parsed_incidents)
     standardized_incidents = []
     parsed_incidents.each do |incident|
-      new_incident = {}
-      new_incident["primary_type"]         = incident.fetch(city_config["primary_type"])
-      new_incident["latitude"]             = incident.fetch(city_config["latitude"])
-      new_incident["longitude"]            = incident.fetch(city_config["longitude"])
-      new_incident["date"]                 = incident.fetch(city_config["date"])
-      new_incident["local_identifier"]     = incident.fetch(city_config["local_identifier"])
-      new_incident["description"]          = incident.fetch(city_config["description"])
-      new_incident["location_description"] = incident.fetch(city_config["location_description"])
-      new_incident["city"]                 = city_config["city"]
-      standardized_incidents << new_incident
+      standardized_incidents << {
+        "primary_type"         => incident.fetch(city_config["primary_type"]),
+        "latitude"             => incident.fetch(city_config["latitude"]),
+        "longitude"            => incident.fetch(city_config["longitude"]),
+        "date"                 => incident.fetch(city_config["date"]),
+        "local_identifier"     => incident.fetch(city_config["local_identifier"]),
+        "description"          => incident.fetch(city_config["description"]),
+        "location_description" => incident.fetch(city_config["location_description"]),
+        "city"                 => city_config["city"]
+      }
     end
     standardized_incidents
   end
