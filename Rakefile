dc822caca2156652793b3c0bc96593ca7295a341
Andrew Kelly <akelly@8thlight.com>
2014-05-03 12:16:15 -0500

    rake task for adding to the database

--- Rakefile
+++ Rakefile
@@ -1,7 +1,11 @@
 require './lib/incident_monger'
+require './lib/incident_creator'
 
 task :gather_incidents do
-  IncidentMonger.fetch_chicago_incidents
+  chicago_incidents = IncidentMonger.fetch_chicago_incidents
+  seattle_incidents = IncidentMonger.fetch_seattle_incidents
+  IncidentCreator.create_chicago_incidents(chicago_incidents)
+  IncidentCreator.create_chicago_incidents(seattle_incidents)
 end
 
 task :assign_locations do
--- lib/incident_monger.rb
+++ lib/incident_monger.rb
@@ -7,13 +7,13 @@ class IncidentMonger
   def self.standardize_incidents(city_config, parsed_incidents)
     parsed_incidents.inject([]) do |acc, incident|
       acc << {
-        "primary_type"         => incident.fetch(city_config["primary_type"]),
+        "primary_type"         => incident.fetch(city_config["primary_type"], ""),
         "latitude"             => incident.fetch(city_config["latitude"], ""),
         "longitude"            => incident.fetch(city_config["longitude"], ""),
-        "date"                 => incident.fetch(city_config["date"]),
-        "local_identifier"     => incident.fetch(city_config["local_identifier"]),
-        "description"          => incident.fetch(city_config["description"]),
-        "location_description" => incident.fetch(city_config["location_description"]),
+        "date"                 => incident.fetch(city_config["date"], ""),
+        "local_identifier"     => incident.fetch(city_config["local_identifier"], ""),
+        "description"          => incident.fetch(city_config["description"], ""),
+        "location_description" => incident.fetch(city_config["location_description"], ""),
         "city"                 => city_config["city"]
       }
     end
@@ -47,11 +47,21 @@ class IncidentMonger
   end
 
   def self.fetch_chicago_incidents
-    # TODO: generalize this to consume configs for different URLs
-    # need to figure out how were going to query this shit
+    p "making request for Chicago incidents..."
     raw_incidents = HTTParty.get("https://data.cityofchicago.org/resource/qnmj-8ku6.json?$limit=1000&$offset=0")
+    p "parsing Chicago incidents..."
     parsed_incidents = JSON.parse(raw_incidents.body)
+    p "standardizing Chicago incidents..."
     standard_incidents = self.standardize_incidents(self.chicago_config, parsed_incidents)
+  end
+
+  def self.fetch_seattle_incidents
+    p "making request for Seattle incidents..."
+    raw_incidents = HTTParty.get("https://data.seattle.gov/resource/7ais-f98f.json?$limit=1000&$offset=0")
+    p "parsing Seattle incidents..."
+    parsed_incidents = JSON.parse(raw_incidents.body)
+    p "standardizing Seattle incidents..."
+    standard_incidents = self.standardize_incidents(self.seattle_config, parsed_incidents)
   end
 
   def self.chicago_config
