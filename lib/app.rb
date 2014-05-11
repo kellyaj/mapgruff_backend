430f7ff1015d6e69a3300cfd67d8a7a9f65de95c
Andrew Kelly <kellyaj@gmail.com>
2014-05-10 20:28:58 -0500

    includes basic tests for app

--- lib/app.rb
+++ lib/app.rb
@@ -6,13 +6,11 @@ require './dmconfig'
 class MapGruffBackend < Sinatra::Base
   get '/all_chicago' do
     chicago_incidents = Incident.all(:city => "Chicago IL")
-    p "Serving up #{chicago_incidents.count} Chicago incidents..."
     chicago_incidents.to_json
   end
 
   get '/all_seattle' do
     seattle_incidents = Incident.all(:city => "Seattle WA")
-    p "Serving up #{seattle_incidents.count} Seattle incidents..."
     seattle_incidents.to_json
   end
 end
---
+++ spec/app_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+require 'app'
+require 'rack/test'
+
+describe 'MapGruffBackend' do
+  include Rack::Test::Methods
+
+  def app
+    MapGruffBackend
+  end
+
+  before(:each) do
+    @chicago_incident = {"city" => "Chicago IL", "primary_type" => "Theft", "local_identifier" => "some_number"}
+    another_chicago_incident = {"city" => "Chicago IL", "primary_type" => "Theft", "local_identifier" => "some_other_chicago_number"}
+    @seattle_incident = {"city" => "Seattle WA", "primary_type" => "Burglary", "local_identifier" => "some_other_number"}
+
+    Incident.create(@chicago_incident)
+    Incident.create(another_chicago_incident)
+    Incident.create(@seattle_incident)
+  end
+
+  it "returns all chicago incidents" do
+    get '/all_chicago'
+
+    incidents = JSON.parse(last_response.body)
+    first_found_incident = incidents.first
+
+    last_response.should be_ok
+    incidents.count.should == 2
+
+    first_found_incident["city"].should == @chicago_incident["city"]
+    first_found_incident["primary_type"].should == @chicago_incident["primary_type"]
+    first_found_incident["local_identifier"].should == @chicago_incident["local_identifier"]
+  end
+
+  it "returns all seattle incidents" do
+    get '/all_seattle'
+
+    incidents = JSON.parse(last_response.body)
+    first_found_incident = incidents.first
+
+    last_response.should be_ok
+    incidents.count.should == 1
+
+    first_found_incident["city"].should == @seattle_incident["city"]
+    first_found_incident["primary_type"].should == @seattle_incident["primary_type"]
+    first_found_incident["local_identifier"].should == @seattle_incident["local_identifier"]
+  end
+end
