197f9094c9b36bfa33196212b05000de6dcb8242
Andrew Kelly <akelly@8thlight.com>
2014-08-23 19:10:44 -0500

    enabling CORS

--- lib/app.rb
+++ lib/app.rb
@@ -5,16 +5,19 @@ require './dmconfig'
 
 class MapGruffBackend < Sinatra::Base
   get '/all_chicago' do
+    response['Access-Control-Allow-Origin'] = "*"
     chicago_incidents = Incident.all(:city => "Chicago IL")
     chicago_incidents.to_json
   end
 
   get '/all_seattle' do
+    response['Access-Control-Allow-Origin'] = "*"
     seattle_incidents = Incident.all(:city => "Seattle WA")
     seattle_incidents.to_json
   end
 
   get '/within_coords' do
+    response['Access-Control-Allow-Origin'] = "*"
     incidents = Incident.all(
       :longitude.lte => params[:north_long],
       :longitude.gte => params[:south_long],
